08d373448bafa873c32f0d18bd7b16b6d5032148
DezkaReid <elmaildeldezkareid@gmail.com>
2019-03-27 18:48:02 -0600

    Added App component

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { HomePage } from '../pages/HomePage/HomePage'
+
+class App extends Component {
+  render () {
+    return (
+      <HomePage title='pagina principal' />
+    )
+  }
+}
+
+export default App
