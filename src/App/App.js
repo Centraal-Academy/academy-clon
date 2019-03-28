e02103679c27a839dd8d209a07fa6357114935d2
DezkaReid <elmaildeldezkareid@gmail.com>
2019-03-27 21:47:28 -0600

    added routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import { HomePage } from '../pages/HomePage/HomePage'
+import AboutPage from '../pages/AboutPage/AboutPage'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+const HomePageWrapper = () => (<HomePage title='Pagina principa' />)
 class App extends Component {
   render () {
     return (
-      <HomePage title='pagina principal' />
+      <Router>
+        <Route path='/' exact component={HomePageWrapper} />
+        <Route path='/about/' component={AboutPage} />
+      </Router>
     )
   }
 }
