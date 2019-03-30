69890e5b9910472dcb8ca833d202e0b7df179387
DezkaReid <elmaildeldezkareid@gmail.com>
2019-03-29 18:39:55 -0600

    Added Switch Router

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import { HomePage } from '../pages/HomePage/HomePage'
 import AboutPage from '../pages/AboutPage/AboutPage'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const HomePageWrapper = () => (<HomePage title='Pagina principa' />)
 class App extends Component {
   render () {
     return (
       <Router>
-        <Route path='/' exact component={HomePageWrapper} />
-        <Route path='/about/' component={AboutPage} />
+        <Switch>
+          <Route path='/' exact component={HomePageWrapper} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/:anything' component={AboutPage} />
+        </Switch>
       </Router>
     )
   }
