a8065eaa414ad8419c29d9d8d10940b97cc87db1
DezkaReid <elmaildeldezkareid@gmail.com>
2019-03-29 20:25:11 -0600

    component to protect views

---
+++ src/components/dumps/ProtectComponent/ProtectComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import FirebaseAdapter from '../../../modules/FirebaseAdapter'
+import LoginButton from '../../smarts/LoginButton/LoginButton'
+
+const authFirebase = FirebaseAdapter.getAuth()
+
+class ProtectComponent extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      auth: false
+    }
+  }
+
+  componentDidMount () {
+    this.unSuscribe = authFirebase().onAuthStateChanged((auth) => {
+      if (auth) {
+        this.setState({ auth: true })
+      } else {
+        this.setState({ auth: false })
+      }
+    })
+  }
+
+  componentWillUnmount () {
+    this.unSuscribe()
+  }
+  render () {
+    if (this.state.auth) {
+      const Component = this.props.component
+      return <Component />
+    }
+    return (<LoginButton />)
+  }
+}
+
+export default ProtectComponent
