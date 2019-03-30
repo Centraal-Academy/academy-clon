dd9e880559335dedec1a93e5a107cde5ba99dbdf
DezkaReid <elmaildeldezkareid@gmail.com>
2019-03-29 18:29:14 -0600

    added destructuring props in component

--- src/components/dumps/MenuBarItem/MenuBarItem.js
+++ src/components/dumps/MenuBarItem/MenuBarItem.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './MenuBarItem.css'
 export default function MenuBarItem (props) {
-  if (!props.url) {
+  if (!props.path) {
     return (
       <span className='MenuBarItem MenuBarItem--disabled'>
         {props.children}
       </span>
     )
   }
+
   return (
-    <a className='MenuBarItem MenuBarItem--enabled' href={props.url} >
+    <a className='MenuBarItem MenuBarItem--enabled' href={props.path} {...props}>
       {props.children}
     </a>
   )
